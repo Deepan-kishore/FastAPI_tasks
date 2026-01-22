41a43599bc625c83f9ae547f5d46c6937cb48395
Deepan Kishore <dpnkishore@gmail.com>
2026-01-22 09:15:28 +0530

    Day 1 - Python basic functions

--- python_basic_tasks.py
+++ python_basic_tasks.py
@@ -1,28 +1,28 @@
-# # 1. Swap variables: a, b = 10, 20 → print swapped
-# a, b = 8, 9
-# print(f"before swap a : {a} , b: {b}")
-# a, b = b, a
-# print(f"after swap a: {a} , b: {b}")
+# 1. Swap variables: a, b = 10, 20 → print swapped
+a, b = 8, 9
+print(f"before swap a : {a} , b: {b}")
+a, b = b, a
+print(f"after swap a: {a} , b: {b}")
 
-# # 2. Max of 3 numbers
-# a, b, c = 1, 2, 3
-# print(max(a, b, c))
-# print(max([a, b, c]))
-
-
-# # 3. Factorial recursive
-# def factorial(n):
-#     if n == 1:
-#         return 1
-#     else:
-#         return n * factorial(n - 1)
-# factorial(5)
+# 2. Max of 3 numbers
+a, b, c = 1, 2, 3
+print(max(a, b, c))
+print(max([a, b, c]))
 
 
-# # 4. Filter even numbers from list
-# random_numbers = range(0, 300)
-# even_list = [n for n in random_numbers if n % 2 == 0]
-# print(even_list)
+# 3. Factorial recursive
+def factorial(n):
+    if n == 1:
+        return 1
+    else:
+        return n * factorial(n - 1)
+factorial(5)
+
+
+# 4. Filter even numbers from list
+random_numbers = range(0, 300)
+even_list = [n for n in random_numbers if n % 2 == 0]
+print(even_list)
 
 
 # 5. Simple calculator (add/sub/mul/div)
